fde04dcaf3ac4f0c8f64af580e647d0c913abfaa
GMReact <64990611+GmReact@users.noreply.github.com>
2020-05-08 14:14:30 -0400

    Added ReactNative Shell project with required OSS libraries

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Hello World!</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
